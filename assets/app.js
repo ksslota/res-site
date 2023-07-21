1a10e7348225ec07532b60beb0a1061ae2e78d25
ksslota <140202394+ksslota@users.noreply.github.com>
2023-07-21 15:16:12 -0400

    Add app.js via upload
    Uploaded the JavaScript file that handles the page's interactivity and responsiveness.

---
+++ assets/app.js
@@ -0,0 +1,45 @@
+// Function to check if an element is in the viewport
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return rect.top < window.innerHeight && rect.bottom >= 0;
+  }
+
+  // Function to handle the fade-in effect for sections
+  function handleScroll() {
+    const sections = document.querySelectorAll('section');
+    sections.forEach(section => {
+      const rect = section.getBoundingClientRect();
+      const inView = rect.top < window.innerHeight && rect.bottom >= 0;
+      if (inView) {
+        section.classList.add('in-view');
+      } else {
+        section.classList.remove('in-view');
+      }
+    });
+  }
+
+  // Attach the scroll event listener to apply the fade-in effect
+  window.addEventListener('scroll', handleScroll);
+
+  // Call handleScroll on page load to apply initial animations
+  window.addEventListener('load', handleScroll);
+
+  // Function to handle the fade-in effect for separators
+  function handleSeparatorScroll() {
+    const separators = document.querySelectorAll('.separator');
+    separators.forEach(separator => {
+      const rect = separator.getBoundingClientRect();
+      const inView = rect.top < window.innerHeight && rect.bottom >= 0;
+      if (inView) {
+        separator.classList.add('in-view');
+      } else {
+        separator.classList.remove('in-view');
+      }
+    });
+  }
+
+  // Attach the scroll event listener to apply the fade-in effect to separators
+  window.addEventListener('scroll', handleSeparatorScroll);
+
+  // Call handleSeparatorScroll on page load to apply initial animations
+  window.addEventListener('load', handleSeparatorScroll);
